c0094bf94d45ee602a9ce0759bedd344eaebac03
fdehech <81689474+fdehech@users.noreply.github.com>
2024-08-26 14:48:54 +0100

    Cleaned up Code

--- script.js
+++ script.js
@@ -1,72 +1,60 @@
+let X,Y;
 const body = document.body
+const shot = new Audio('shot.mp3');
+const load = new Audio('load.mp3');
+
+var Diff=700;
+var Score = 0;
+
+var ScoreBoard=Object.assign(document.createElement("span"),{
+    className:"Score",
+    innerText:Score
+});
+
+
+let count = Object.assign(document.createElement('p'),{
+    className:'Countdown'
+});
+
+const cls = () => {
+    while(body.querySelector(".Landing")){
+        body.querySelector(".Landing").remove();
+    }
+}
+
+const random = (min,max) =>  {
+    return (Math.floor((Math.random())*(max-min+1))+min);
+}
 
 let start = () => {
     load.play();
     cls();
-    countdown(3,GAMEON);
+    countdown(3,starting = () =>{
+        body.append(ScoreBoard);
+        Bullseye();
+    });
 }
 
-let startbtn = Object.assign(document.createElement("span"),{onclick:start});
-startbtn.innerText="Start";
-startbtn.classList.add("Start");
-startbtn.classList.add("Landing");
+let startbtn = Object.assign(document.createElement("span"),{
+    onclick:start,
+    className: "Start Landing",
+    innerText:"Start"
+});
+
 body.append(startbtn);
 
+let ball = Object.assign(document.createElement("span"),{
+    className:"Bullseye",
+});
 
+Object.assign(ball.style, {
+    width: '75px',
+    height: '75px'
+});
 
-let ball = document.createElement("span");
-ball.classList.add("Bullseye")
-ball.style.width='75px';
-ball.style.height='75px';
-ball.setAttribute("onmouseenter","Increment()");
+ball.addEventListener("mouseover",Increment);
 body.append(ball);
 
-
-let Diff=700;
-let Score = 0;
-let ScoreBoard=document.createElement("span");
-ScoreBoard.classList.add("Score");
-ScoreBoard.innerText=Score;
-let count = document.createElement('p');
-count.classList.add("Countdown");
-
-let shot = new Audio('shot.mp3');
-let load = new Audio('load.mp3');
-
-
-let X,Y;
-let cls = () => {
-    while(body.querySelector(".Landing")){
-        body.querySelector(".Landing").remove();
-    }
-
-}
-
-let random = (min,max) =>  {
-    return (Math.floor((Math.random())*(max-min+1))+min);
-}
-
-let countdown = (time,callback) => {
-
-    count.innerText=time;
-    body.append(count);
-    if(time==0){
-        count.remove();
-        callback();
-        return 0;
-    }
-    
-    setTimeout(() => {
-        countdown(time - 1,callback);
-    }, 1000); 
-}
-
-
-
-let GAMEON = () => {
-    body.append(ScoreBoard);
-    Bullseye();
-}
 
 let Bullseye = () => {
 
@@ -85,52 +73,66 @@ let Bullseye = () => {
     },Diff);
 
 }
+
+let countdown = (time,callback) => {
+
+    count.innerText=time;
+    body.append(count);
+    if(time==0){
+        count.remove();
+        callback();
+        return 0;
+    }
+    
+    setTimeout(() => {
+        countdown(time - 1,callback);
+    }, 1000); 
+}
+
 let Increment = () => {
     shot.play();
     Score++;
     ScoreBoard.innerText=Score;
     ball.remove();
-    if (Score == 10){
-        Diff=600;
-        ball.style.width='60px';
-        ball.style.height='60px';
-    }
-    else if (Score == 20){
-        Diff=580;
-        ball.style.width='55px';
-        ball.style.height='55px';
-    }
-    else if (Score == 30){
-        Diff=540;
-        ball.style.width='50px';
-        ball.style.height='50px';
-    }
-    else if (Score == 40){
-        ball.style.width='45px';
-        ball.style.height='45px';
-    }
-    else if (Score == 50){
-        Diff=520;
-        ball.style.width='40px';
-        ball.style.height='40px';
-    }
-    else if (Score == 60){
-        Diff=510;
-        ball.style.width='35px';
-        ball.style.height='35px';
-    }
-    else if (Score == 70){
-        Diff=500;
-        ball.style.width='30px';
-        ball.style.height='30px';
-    }
-    else if (Score == 80){
-        Diff=500;
-        ball.style.width='20px';
-        ball.style.height='20px';
-    }else if (Score == 100){
-        Diff=480;
-        ball.style.width='15px';
-        ball.style.height='15px';
+    switch (Score) {
+        case 10:
+            Diff = 600;
+            ball.style.width = '60px';
+            ball.style.height = '60px';
+            break;
+        case 20:
+            Diff = 580;
+            ball.style.width = '55px';
+            ball.style.height = '55px';
+            break;
+        case 30:
+            Diff = 540;
+            ball.style.width = '50px';
+            ball.style.height = '50px';
+            break;
+        case 40:
+            ball.style.width = '45px';
+            ball.style.height = '45px';
+            break;
+        case 50:
+            Diff = 520;
+            ball.style.width = '40px';
+            ball.style.height = '40px';
+            break;
+        case 60:
+            Diff = 510;
+            ball.style.width = '35px';
+            ball.style.height = '35px';
+            break;
+        case 70:
+            Diff = 500;
+            ball.style.width = '30px';
+            ball.style.height = '30px';
+            break;
+        case 80:
+            Diff = 500;
+            ball.style.width = '20px';
+            ball.style
+    
     }
 }
